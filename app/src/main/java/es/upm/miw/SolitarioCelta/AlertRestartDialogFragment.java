0ca2231cc2f18a89c6dcbd88c93b6e2ace1ef026
Sergio Cabrero <s.cabrerof4@gmail.com>
2019-10-25 20:12:59 +0200

    Add restart

---
+++ app/src/main/java/es/upm/miw/SolitarioCelta/AlertRestartDialogFragment.java
@@ -0,0 +1,42 @@
+package es.upm.miw.SolitarioCelta;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class AlertRestartDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final MainActivity main = (MainActivity) getActivity();
+
+        super.onCreateDialog(savedInstanceState);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(main);
+        builder
+                .setTitle(R.string.txtDialogoReiniciarTitulo)
+                .setMessage(R.string.txtDialogoReiniciarPregunta)
+                .setPositiveButton(
+                        getString(R.string.txtDialogoReiniciarAfirmativo),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                main.miJuego.reiniciar();
+                                main.mostrarTablero();
+                            }
+                        }
+                )
+                .setNegativeButton(
+                        getString(R.string.txtDialogoReiniciarNegativo),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                getDialog().cancel();
+                            }
+                        }
+                );
+
+        return builder.create();
+    }
+}
--- app/src/main/java/es/upm/miw/SolitarioCelta/MainActivity.java
+++ app/src/main/java/es/upm/miw/SolitarioCelta/MainActivity.java
@@ -82,7 +82,9 @@ public class MainActivity extends AppCompatActivity {
             case R.id.opcAcercaDe:
                 startActivity(new Intent(this, AcercaDe.class));
                 return true;
-
+            case R.id.opcReiniciarPartida:
+                new AlertRestartDialogFragment().show(getFragmentManager(), "ALERT_DIALOG");
+                return true;
             // TODO!!! resto opciones
 
             default:
